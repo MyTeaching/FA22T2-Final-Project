c47cdcf642e92abdf100af6018183668e35ef4ba
cchanyu <chanyu96@gmail.com>
2022-11-26 20:39:29 -0500

    CC - games fundamental set

--- FinalProject/app/src/main/java/com/example/finalproject/BattleActivity.java
+++ FinalProject/app/src/main/java/com/example/finalproject/BattleActivity.java
@@ -1,28 +1,86 @@
 package com.example.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /*
-* HERE AFTER USER CHOSE HERO + THEIR MOVE
-* ENEMY WILL SHOW?
+* This is where you'll see your enemy
+* If you choose to run away -> goes back to MainActivity.java
 *
-* WE SAVE HP AND MOVE ACTIVITY
+* IF you choose to fight -> sends user to MovesActivity.java
+*
 *
 * */
 
 public class BattleActivity extends AppCompatActivity {
-    private TextView usersHeroInfo;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private TextView textView_My_Hero_Name, textView_My_Hero_HP;
+    private TextView textView_Enemy_Hero_Name, textView_Enemy_Hero_HP;
+    private ImageView imageView_My_Hero, imageView_Enemy_Hero;
+
+    private String heroChosen;
+    private int heroValue, heroHP, heroMP;
+
+    private String enemyChosen;
+    private int enemyValue, enemyHP, enemyMP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_battle);
 
-        //This is the views for the activity_battle.xml
-        //this'll be the one displaying the heroName then the move the user chose for it
-        usersHeroInfo = (TextView) findViewById(R.id.usersChoice);
+        Bundle extras = getIntent().getExtras();
+        heroChosen = extras.getString("HeroChosen");
+        heroValue = extras.getInt("HeroValue");
+        heroHP = extras.getInt("HeroHP");
+        heroMP = extras.getInt("HeroMP");
 
+        enemyChosen = extras.getString("EnemyChosen");
+        enemyValue = extras.getInt("EnemyValue");
+        enemyHP = extras.getInt("EnemyHP");
+        enemyMP = extras.getInt("EnemyMP");
+
+        textView_My_Hero_Name = (TextView) findViewById(R.id.textView_My_Hero_Name);
+        textView_My_Hero_HP = (TextView) findViewById(R.id.textView_My_Hero_HP);
+        imageView_My_Hero = (ImageView) findViewById(R.id.imageView_My_Hero);
+        textView_My_Hero_Name.setText(heroChosen);
+        textView_My_Hero_HP.setText("HP: " + String.valueOf(heroHP));
+        int hero_id = getResources().getIdentifier(heroChosen, "drawable", getPackageName());
+        imageView_My_Hero.setImageResource(hero_id);
+
+        textView_Enemy_Hero_Name = (TextView) findViewById(R.id.textView_Enemy_Hero_Name);
+        textView_Enemy_Hero_HP = (TextView) findViewById(R.id.textView_Enemy_Hero_HP);
+        imageView_Enemy_Hero = (ImageView) findViewById(R.id.imageView_Enemy_Hero);
+        textView_Enemy_Hero_Name.setText(enemyChosen);
+        textView_Enemy_Hero_HP.setText("HP: " + String.valueOf(enemyHP));
+        int enemy_id = getResources().getIdentifier(enemyChosen, "drawable", getPackageName());
+        imageView_Enemy_Hero.setImageResource(enemy_id);
+
+    }
+
+    public void onClickFight(View view){
+        Intent intent = new Intent(this, MovesActivity.class);
+        intent.putExtra("HeroChosen", heroChosen);
+        intent.putExtra("HeroValue", heroValue);
+        intent.putExtra("HeroHP", heroHP);
+        intent.putExtra("HeroMP", heroMP);
+
+        intent.putExtra("EnemyChosen", enemyChosen);
+        intent.putExtra("EnemyValue", enemyValue);
+        intent.putExtra("EnemyHP", enemyHP);
+        intent.putExtra("EnemyMP", enemyMP);
+        startActivity(intent);
+    }
+
+    // If user chickens away, it should add a Loss to their record
+    public void onClickRun(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- FinalProject/app/src/main/java/com/example/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,170 +1,163 @@
 package com.example.finalproject;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+
 /*
- * WE PICK OUR CHARACTER HERE
+ * MainActivity (Starting Point aka where we pick our character)
+ * once the character is chosen it'll be passed to BattleActivity.java
  *
- * SETTINGS TO CHANGE OUR HERO WE CHOSE
+ * We need to add Win/Loss system.
  * */
 
 public class MainActivity extends AppCompatActivity {
-    //this is the string that will be called in the intent for the second activity
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     private String heroChosen;
-    private TextView move1;
-    private TextView move2;
-    private TextView move3;
-    //this will be assigned to the name from the textviews I'll be adding
-    //these textviews will be under the heros image. it'll be used to identify them
-    //feel free to add them yourself if you want. I already started to implement it for Genji
-    private TextView heroName;
+    private int heroValue, heroHP, heroMP;
 
+    private String enemyChosen;
+    private int enemyValue, enemyHP, enemyMP;
 
+    private String[] hero_name, hero_HP, hero_MP;
+    private ArrayList hero_list_name, hero_list_hp, hero_list_mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        hero_name = res.getStringArray(R.array.hero_name);
+        hero_HP = res.getStringArray(R.array.hero_HP);
+        hero_MP = res.getStringArray(R.array.hero_MP);
+
+        hero_list_name = new ArrayList<String>(Arrays.asList(hero_name));
+        hero_list_hp = new ArrayList<String>(Arrays.asList(hero_HP));
+        hero_list_mp = new ArrayList<String>(Arrays.asList(hero_MP));
+
+        Log.d(TAG, "ArrayList Names: " + hero_list_name);
+        Log.d(TAG, "ArrayList HP: " + hero_list_hp);
+        Log.d(TAG, "ArrayList MP: " + hero_list_mp);
     }
 
-    //ON CLICK METHOD FOR GENJI WHICH CALLS ON THE SECOND ACTIVITY(MOVESACTIVITY)
-    public void genjiMoves(View view){
-        //if statement for the hero chosen
-        heroChosen = "genji";
-        launchMovesActivity();
-    }
-
-    //ON CLICK METHOD FOR MEI WHICH CALLS ON THE SECOND ACTIVITY
-    public void meiMoves(View view){
-        //if statement for the hero chosen
-        if(heroChosen == "mei") {
-            //making the strings for the moves that'll be displayed for the character chosen
-            String meimove1 = "ENDOTHERMIC BLASTER";
-            String meimove2 = "ICE WALL OFF MAP";
-            String meimove3 = "CAST A BLIZZARD TO FREEZE ";
-            //giving value to the buttons in the moves.xml
-            //the values will be specified to the moves the hero chosen can do
-            move1.setText(meimove1);
-            move2.setText(meimove2);
-            move3.setText(meimove3);
-            launchMovesActivity();
+    public void onClickMoves(View view) {
+        switch (view.getId()) {
+            case  R.id.genji: {
+                heroValue = 0;
+                findHero(heroValue);
+                break;
+            }
+            case R.id.mei: {
+                heroValue = 1;
+                findHero(heroValue);
+                break;
+            }
+            case R.id.moira: {
+                heroValue = 2;
+                findHero(heroValue);
+                break;
+            }
+            case R.id.cassidy: {
+                heroValue = 3;
+                findHero(heroValue);
+                break;
+            }
+            case R.id.roadhog: {
+                heroValue = 4;
+                findHero(heroValue);
+                break;
+            }
+            case R.id.reaper: {
+                heroValue = 5;
+                findHero(heroValue);
+                break;
+            }
+            case R.id.mercy: {
+                heroValue = 6;
+                findHero(heroValue);
+                break;
+            }
+            case R.id.widow: {
+                heroValue = 7;
+                findHero(heroValue);
+                break;
+            }
+            default:
+                break;
         }
     }
 
-    //ON CLICK METHOD FOR MOIRA WHICH CALLS ON THE SECOND ACTIVITY
-    public void moiraMoves(View view){
-        //if statement for the hero chosen
-        if(heroChosen == "moira") {
-            //making the strings for the moves that'll be displayed for the character chosen
-            String moiramove1 = "SOUL SUCK";
-            String moiramove2 = "CAST BIOTIC ORB";
-            String moiramove3 = "COALESCENCE";
-            //giving value to the buttons in the moves.xml
-            //the values will be specified to the moves the hero chosen can do
-            move1.setText(moiramove1);
-            move2.setText(moiramove2);
-            move3.setText(moiramove3);
-            launchMovesActivity();
-        }
+    private void findHero(int heroValue) {
+        heroChosen = (String) hero_list_name.get(heroValue);
+        heroHP = ArraySearchHP(heroValue);
+        heroMP = ArraySearchMP(heroValue);
+
+        Log.d(TAG, "Hero Name: " + heroChosen);
+        Log.d(TAG, "Hero HP: " + heroHP);
+        Log.d(TAG, "Hero MP: " + heroMP);
+
+        findEnemy();
     }
 
-    //ON CLICK METHOD FOR CASSIDY WHICH CALLS ON THE SECOND ACTIVITY
-    public void cassidyMoves(View view){
-        //if statement for the hero chosen
-        if(heroChosen == "cassidy") {
-            //making the strings for the moves that'll be displayed for the character chosen
-            String cassidymove1 = "PEACEKEEPER";
-            String cassidymove2 = "MAGNETIC GRENADE";
-            String cassidymove3 = "DEADEYE - IT'S HIGH NOON";
-            //giving value to the buttons in the moves.xml
-            //the values will be specified to the moves the hero chosen can do
-            move1.setText(cassidymove1);
-            move2.setText(cassidymove2);
-            move3.setText(cassidymove3);
-            launchMovesActivity();
-        }
+    public void findEnemy() {
+        Random random = new Random();
+        enemyValue = random.nextInt(7);
+
+        enemyChosen = (String) hero_list_name.get(enemyValue);
+        enemyHP = ArraySearchHP(enemyValue);
+        enemyMP = ArraySearchMP(enemyValue);
+
+        Log.d(TAG, "Enemy Name: " + enemyChosen);
+        Log.d(TAG, "Enemy HP: " + enemyHP);
+        Log.d(TAG, "Enemy MP: " + enemyMP);
+
+        launchBattle();
     }
 
-    //ON CLICK METHOD FOR ROADHOG WHICH CALLS ON THE SECOND ACTIVITY
-    public void roadhogMoves(View view){
-        //if statement for the hero chosen
-        if(heroChosen == "roadhog") {
-            //making the strings for the moves that'll be displayed for the character chosen
-            String roadhogmove1 = "SCRAP GUN";
-            String roadhogmove2 = "CHAIN HOOK";
-            String roadhogmove3 = "WHOLE HOG";
-            //giving value to the buttons in the moves.xml
-            //the values will be specified to the moves the hero chosen can do
-            move1.setText(roadhogmove1);
-            move2.setText(roadhogmove2);
-            move3.setText(roadhogmove3);
-            launchMovesActivity();
-        }
+//     Chanyu - Notes
+//     If anyone doesn't understand why I implemented this:
+
+//     Array Search Function was necessary, because
+//     ArrayList<Integer>.asList only returns 1st index (when I need the entire list)
+//     so I didn't want to loop the entire ArrayList, just to grab an int
+//     since String had no issues with .asList(),
+//     I grabbed everything as String, then converted to int
+    public int ArraySearchHP(int index) {
+        String temp_string = (String) hero_list_hp.get(index);
+        return Integer.valueOf(temp_string);
     }
 
-    //ON CLICK METHOD FOR REAPER WHICH CALLS ON THE SECOND ACTIVITY
-    public void reaperMoves(View view){
-        //if statement for the hero chosen
-        if(heroChosen == "reaper") {
-            //making the strings for the moves that'll be displayed for the character chosen
-            String reapermove1 = "HELLFIRE SHOTGUNS";
-            String reapermove2 = "THE REAPING";
-            String reapermove3 = "DEATH BLOSSOM";
-            //giving value to the buttons in the moves.xml
-            //the values will be specified to the moves the hero chosen can do
-            move1.setText(reapermove1);
-            move2.setText(reapermove2);
-            move3.setText(reapermove3);
-            launchMovesActivity();
-        }
+    public int ArraySearchMP(int index) {
+        String temp_string = (String) hero_list_mp.get(index);
+        return Integer.valueOf(temp_string);
     }
 
-    //ON CLICK METHOD FOR MERCY WHICH CALLS ON THE SECOND ACTIVITY
-    public void mercyMoves(View view){
-        //if statement for the hero chosen
-        if(heroChosen == "mercy") {
-            //making the strings for the moves that'll be displayed for the character chosen
-            String mercymove1 = "CADUCEUS BLASTER";
-            String mercymove2 = "RESURRECT";
-            String mercymove3 = "VALKYRIE";
-            //giving value to the buttons in the moves.xml
-            //the values will be specified to the moves the hero chosen can do
-            move1.setText(mercymove1);
-            move2.setText(mercymove2);
-            move3.setText(mercymove3);
-            launchMovesActivity();
-        }
-    }
-
-    //ON CLICK METHOD FOR WIDOW WHICH CALLS ON THE SECOND ACTIVITY
-    public void widowMoves(View view){
-        //if statement for the hero chosen
-        if(heroChosen == "widow") {
-            //making the strings for the moves that'll be displayed for the character chosen
-            String widowmove1 = "WIDOW'S KISS";
-            String widowmove2 = "VENOM MINE";
-            String widowmove3 = "INFRA-SIGHT";
-            //giving value to the buttons in the moves.xml
-            //the values will be specified to the moves the hero chosen can do
-            move1.setText(widowmove1);
-            move2.setText(widowmove2);
-            move3.setText(widowmove3);
-            launchMovesActivity();
-        }
-    }
-
-
-    //launch moves activity which calls upon the second activity
-   public void launchMovesActivity(){
-        //sends intent from this class to the moves class
+    // After character selected + enemy found, this will be triggered
+    public void launchBattle() {
         Intent intent = new Intent(this, BattleActivity.class);
-        //calls back the heros choice and the moves will be shown via the choice the user makes
-        intent.putExtra("HeroChoice", heroChosen);
-        //starting the moves activity
+        intent.putExtra("HeroChosen", heroChosen);
+        intent.putExtra("HeroValue", heroValue);
+        intent.putExtra("HeroHP", heroHP);
+        intent.putExtra("HeroMP", heroMP);
+
+        intent.putExtra("EnemyChosen", enemyChosen);
+        intent.putExtra("EnemyValue", enemyValue);
+        intent.putExtra("EnemyHP", enemyHP);
+        intent.putExtra("EnemyMP", enemyMP);
+
         startActivity(intent);
     }
 }
