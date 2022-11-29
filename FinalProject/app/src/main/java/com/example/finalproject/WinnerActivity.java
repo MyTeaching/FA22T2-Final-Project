afdc505b2952e9f8e2aa553d315a81529f14ebff
cchanyu <chanyu96@gmail.com>
2022-11-29 16:01:35 -0500

    CC - notification system added

--- FinalProject/app/src/main/java/com/example/finalproject/WinnerActivity.java
+++ FinalProject/app/src/main/java/com/example/finalproject/WinnerActivity.java
@@ -1,9 +1,15 @@
 package com.example.finalproject;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,6 +19,12 @@ public class WinnerActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private NotificationManager notificationManager;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel"; //API 26 and above each notification needs a channel id
+    private static final String PRIMARY_CHANNEL_NAME = "my primary notification channel";//API 26 and above each notification needs a channel name
+    private static final int IMPORTANCE_LEVEL = NotificationManager.IMPORTANCE_DEFAULT; //priority level
+    private static final int NOTIFICATION_ID_0 = 0; //unique allows us to update notification
+
     private String heroChosen;
     private int heroValue, heroHP, heroMP;
 
@@ -21,6 +33,7 @@ public class WinnerActivity extends AppCompatActivity {
 
     private ImageView imageView_winner;
     private TextView textView_who_won;
+    private String WonBy_Text;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,15 +55,40 @@ public class WinnerActivity extends AppCompatActivity {
         imageView_winner = (ImageView) findViewById(R.id.imageView_winner);
 
         int WonBy = heroHP - enemyHP;
-        String WonBy_Text = "Your hero " + heroChosen + " won the enemy hero by " + WonBy + " hp!";
+        WonBy_Text = "Your hero " + heroChosen + " won the enemy hero by " + WonBy + " hp!";
         textView_who_won.setText(WonBy_Text);
         int hero_id = getResources().getIdentifier(heroChosen, "drawable", getPackageName());
         imageView_winner.setImageResource(hero_id);
 
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        sendNotification();
     }
 
     public void onClickReturn(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    protected void sendNotification() {
+        NotificationCompat.Builder notificationBuilder;
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, NOTIFICATION_ID_0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        if(Build.VERSION.SDK_INT >= 26){
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID, PRIMARY_CHANNEL_NAME, IMPORTANCE_LEVEL);
+            notificationChannel.setDescription("Reminders");
+            notificationManager.createNotificationChannel(notificationChannel);
+            notificationBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                    .setContentTitle("You've won!")
+                    .setContentText(WonBy_Text)
+                    .setSmallIcon(R.drawable.ic_action_victory);
+        } else {
+            notificationBuilder = new NotificationCompat.Builder(this)
+                    .setPriority(IMPORTANCE_LEVEL)
+                    .setContentTitle("You've won!").setContentText(WonBy_Text)
+                    .setSmallIcon(R.drawable.ic_action_victory);
+        }
+        notificationBuilder.setContentIntent(pendingIntent);
+        notificationManager.notify(NOTIFICATION_ID_0, notificationBuilder.build());
     }
 }
